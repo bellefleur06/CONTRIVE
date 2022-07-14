f3cef834171c61dcb46040e410dc6a5cd1daff96
cielzguerrero <96853751+bellefleur06@users.noreply.github.com>
2022-07-14 17:56:45 +0800

    updated

--- admin/navbar.php
+++ admin/navbar.php
@@ -74,7 +74,17 @@
   								<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z"/>
   								<path fill-rule="evenodd" d="M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
 								</svg>
-					        	<span class="icon-badge">1</span>
+								<?php
+									$sql = "SELECT * FROM orders WHERE notification_status = '0' AND status != 'Received' AND status != 'Returning' AND status != 'Returned'";
+									$result = mysqli_query($conn, $sql);
+									$count = mysqli_num_rows($result);
+								?>
+								<?php if ($count > 0) : ?>
+					        		<span class="icon-badge"><?php echo $count; ?></span>
+								<?php else : ?>
+									<span class="icon-badge"></span>
+								<?php endif; ?>
+
 					        </a><!--//dropdown-toggle-->
 					        
 					        <div class="dropdown-menu p-0" aria-labelledby="notifications-dropdown-toggle">
@@ -82,14 +92,33 @@
 						            <h5 class="dropdown-menu-title mb-0">Notifications</h5>
 						        </div><!--//dropdown-menu-title-->
 						        <div class="dropdown-menu-content">
+								<?php
+									$sql = "SELECT * FROM orders WHERE notification_status = '0' AND status != 'Received' AND status != 'Returning' AND status != 'Returned' ORDER BY id DESC";
+									$result = mysqli_query($conn, $sql);
+									$count = mysqli_num_rows($result);
+
+									//check if suppliers exist
+									if ($count > 0) {
+
+										while ($row = mysqli_fetch_assoc($result)) {
+											$id = $row['id'];
+											$products = $row['products'];
+											$order_id = $row['order_id'];
+											// $person = $row['person'];
+											// $email = $row['email'];
+											// $contact = $row['contact'];
+											// $address = $row['address'];
+											$status = $row['status'];
+									?>
+
 							       <div class="item p-3">
 								        <div class="row gx-2 justify-content-between align-items-center">
 									        <div class="col-auto">
-										       <img class="profile-image" src="../staff_images/clayton-mpDV4xaFP8c-unsplash.jpg" alt="">
+										        <img class="profile-image" src="../staff_images/clayton-mpDV4xaFP8c-unsplash.jpg" alt="">
 									        </div><!--//col-->
 									        <div class="col">
 										        <div class="info"> 
-											        <div class="desc">Carmina updated the project for Jmd Food Sales. Lorem ipsum dolor sit amet. </div>
+											        <div class="desc"> <?php echo "Your Order " . $order_id  . " is " .  $status ; ?></div>
 											        <div class="meta"> 2 hrs ago</div>
 										        </div>
 									        </div><!--//col--> 
@@ -97,6 +126,27 @@
 								        <a class="link-mask" href="notifications.php"></a>
 							       </div><!--//item-->
 							       
+								   <?php
+											}
+										} else {
+									?>
+										<div class="item p-3">
+											<div class="row gx-2 justify-content-between align-items-center">
+												<div class="col-auto">
+													<img class="profile-image" src="../staff_images/clayton-mpDV4xaFP8c-unsplash.jpg" alt="">
+												</div><!--//col-->
+												<div class="col">
+													<div class="info"> 
+														<div class="desc">Carmina updated the project for Jmd Food Sales. Lorem ipsum dolor sit amet. </div>
+														<div class="meta"> 2 hrs ago</div>
+													</div>
+												</div><!--//col--> 
+											</div><!--//row-->
+											<a class="link-mask" href="notifications.php"></a>
+										</div><!--//item-->
+								   <?php
+										}
+									?>
 						        </div><!--//dropdown-menu-content-->
 						        
 						        <div class="dropdown-menu-footer p-2 text-center">
@@ -114,12 +164,18 @@
 									<!-- <span><a href="settings.php"><i class="fas fa-cog" role="button"></i></span> -->
 									<span><a href="settings.php">
 									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M8.837 1.626c-.246-.835-1.428-.835-1.674 0l-.094.319A1.873 1.873 0 0 1 4.377 3.06l-.292-.16c-.764-.415-1.6.42-1.184 1.185l.159.292a1.873 1.873 0 0 1-1.115 2.692l-.319.094c-.835.246-.835 1.428 0 1.674l.319.094a1.873 1.873 0 0 1 1.115 2.693l-.16.291c-.415.764.42 1.6 1.185 1.184l.292-.159a1.873 1.873 0 0 1 2.692 1.116l.094.318c.246.835 1.428.835 1.674 0l.094-.319a1.873 1.873 0 0 1 2.693-1.115l.291.16c.764.415 1.6-.42 1.184-1.185l-.159-.291a1.873 1.873 0 0 1 1.116-2.693l.318-.094c.835-.246.835-1.428 0-1.674l-.319-.094a1.873 1.873 0 0 1-1.115-2.692l.16-.292c.415-.764-.42-1.6-1.185-1.184l-.291.159A1.873 1.873 0 0 1 8.93 1.945l-.094-.319zm-2.633-.283c.527-1.79 3.065-1.79 3.592 0l.094.319a.873.873 0 0 0 1.255.52l.292-.16c1.64-.892 3.434.901 2.54 2.541l-.159.292a.873.873 0 0 0 .52 1.255l.319.094c1.79.527 1.79 3.065 0 3.592l-.319.094a.873.873 0 0 0-.52 1.255l.16.292c.893 1.64-.902 3.434-2.541 2.54l-.292-.159a.873.873 0 0 0-1.255.52l-.094.319c-.527 1.79-3.065 1.79-3.592 0l-.094-.319a.873.873 0 0 0-1.255-.52l-.292.16c-1.64.893-3.433-.902-2.54-2.541l.159-.292a.873.873 0 0 0-.52-1.255l-.319-.094c-1.79-.527-1.79-3.065 0-3.592l.319-.094a.873.873 0 0 0 .52-1.255l-.16-.292c-.892-1.64.902-3.433 2.541-2.54l.292.159a.873.873 0 0 0 1.255-.52l.094-.319z"/>
-  <path fill-rule="evenodd" d="M8 5.754a2.246 2.246 0 1 0 0 4.492 2.246 2.246 0 0 0 0-4.492zM4.754 8a3.246 3.246 0 1 1 6.492 0 3.246 3.246 0 0 1-6.492 0z"/>
-</svg></span>
+										<path fill-rule="evenodd" d="M8.837 1.626c-.246-.835-1.428-.835-1.674 0l-.094.319A1.873 1.873 0 0 1 4.377 3.06l-.292-.16c-.764-.415-1.6.42-1.184 1.185l.159.292a1.873 1.873 0 0 1-1.115 2.692l-.319.094c-.835.246-.835 1.428 0 1.674l.319.094a1.873 1.873 0 0 1 1.115 2.693l-.16.291c-.415.764.42 1.6 1.185 1.184l.292-.159a1.873 1.873 0 0 1 2.692 1.116l.094.318c.246.835 1.428.835 1.674 0l.094-.319a1.873 1.873 0 0 1 2.693-1.115l.291.16c.764.415 1.6-.42 1.184-1.185l-.159-.291a1.873 1.873 0 0 1 1.116-2.693l.318-.094c.835-.246.835-1.428 0-1.674l-.319-.094a1.873 1.873 0 0 1-1.115-2.692l.16-.292c.415-.764-.42-1.6-1.185-1.184l-.291.159A1.873 1.873 0 0 1 8.93 1.945l-.094-.319zm-2.633-.283c.527-1.79 3.065-1.79 3.592 0l.094.319a.873.873 0 0 0 1.255.52l.292-.16c1.64-.892 3.434.901 2.54 2.541l-.159.292a.873.873 0 0 0 .52 1.255l.319.094c1.79.527 1.79 3.065 0 3.592l-.319.094a.873.873 0 0 0-.52 1.255l.16.292c.893 1.64-.902 3.434-2.541 2.54l-.292-.159a.873.873 0 0 0-1.255.52l-.094.319c-.527 1.79-3.065 1.79-3.592 0l-.094-.319a.873.873 0 0 0-1.255-.52l-.292.16c-1.64.893-3.433-.902-2.54-2.541l.159-.292a.873.873 0 0 0-.52-1.255l-.319-.094c-1.79-.527-1.79-3.065 0-3.592l.319-.094a.873.873 0 0 0 .52-1.255l-.16-.292c-.892-1.64.902-3.433 2.541-2.54l.292.159a.873.873 0 0 0 1.255-.52l.094-.319z"/>
+										<path fill-rule="evenodd" d="M8 5.754a2.246 2.246 0 1 0 0 4.492 2.246 2.246 0 0 0 0-4.492zM4.754 8a3.246 3.246 0 1 1 6.492 0 3.246 3.246 0 0 1-6.492 0z"/>
+										</svg></span>
 								</a> &nbsp
 								
+								<?php
 
+									$sql = "SELECT * FROM staffs WHERE id = '{$_SESSION['id']}'";
+									$result = mysqli_query($conn, $sql);
+									$row = mysqli_fetch_assoc($result);
+
+								?>
 								<div class="app-utility-item app-user-dropdown dropdown">
 									<a class="dropdown-toggle" id="user-dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
 										<img src="../staff_images/<?php echo $_SESSION['profile']; ?>" alt="" srcset="" style="border-radius: 50%"></a>
